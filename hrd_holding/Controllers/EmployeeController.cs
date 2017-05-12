4c998f16ad1bd7bc4ca2e1f09dc14c2d17417941
handendari <handendari@gmail.com>
2017-05-12 14:35:55 +0700

    kantor

--- hrd_holding/Controllers/EmployeeController.cs
+++ hrd_holding/Controllers/EmployeeController.cs
@@ -25,48 +25,11 @@ namespace hrd_holding.Controllers
             return View();
         }
 
-        
-        //public dynamic GetEmployeeList(int pCompanyCode, string _search, long nd, int rows, int page, string sidx, string sord = "ASC", string filters = "")
-        //{
-        //    // Get Data
-        //    var vRes = _empService.GetEmployeeList(pCompanyCode,_search, page, rows, sidx, sord, filters);
-        //    var vList = vRes.objResult as IEnumerable<mEmployeeModel>;
+        public ActionResult EmpDetail()
+        {
+            return View("EmployeeDetail");
+        }
 
-        //    var pageIndex = page;
-        //    var pageSize = rows;
-        //    var totalRecords = vRes.total_record;
-        //    var totalPages = (int)Math.Ceiling((float)totalRecords / (float)pageSize);
-
-        //    //vList = vList.Skip(pageIndex * pageSize).Take(pageSize);
-
-        //    return Json(new
-        //    {
-        //        total = totalPages,
-        //        page = page,
-        //        records = totalRecords,
-        //        rows = (
-        //            from vItem in vList
-        //            select new
-        //            {
-        //                id = vItem.employee_code,
-        //                cell = new object[] {
-        //                    vItem.employee_code,
-        //                    vItem.employee_name,
-        //                    vItem.emp_address,
-        //                    vItem.company_name,
-        //                    vItem.department_name,
-        //                    vItem.division_name,
-        //                    vItem.level_name,
-        //                    vItem.entry_date
-        //              }
-        //            }).ToArray()
-        //    }, JsonRequestBehavior.AllowGet);
-        //}
-
-        //public dynamic GetEmployeeList_Lama(int pCompanyCode,int? filterscount=0,int? groupscount=0,int? pagenum=0,
-        //    int pagesize=10,int? recordstartindex=0, int? recordendindex=16,_=14940)
-        //{
-       
         [HttpPost]
        public dynamic GetEmployeeList_Lama(int pCompanyCode)
        {
